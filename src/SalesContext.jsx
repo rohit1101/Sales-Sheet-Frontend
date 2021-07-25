2164e1f6c1947f202cb1b29c1d86575980f3939d
rohit1101 <srohit1101@gmail.com>
2021-07-25 16:11:04 +0530

    Remove SalesContext.jsx

--- src/SalesContext.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SalesContext = React.createContext();
-// export const SalesProvider = SalesContext.Provider;
-// export const SalesConsumer = SalesContext.Consumer;
-
-export default SalesContext;
