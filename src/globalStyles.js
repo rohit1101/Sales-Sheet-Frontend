d7acbae6ab5b1cde2cdb8fa05ecf7b64623536fd
rohit1101 <srohit1101@gmail.com>
2022-02-20 13:25:25 +0530

    style Login.jsx

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,18 +1,23 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components/macro";
 
 export const GlobalStyles = createGlobalStyle`
+
+  @import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&family=Outfit:wght@400;700&display=swap');
+  
   *, *::before, *::after {
     box-sizing: border-box;
   }
   html {
-    font-size: 1.125rem;
+    font-size: 16px;
     height: 100%;
   }
 
   body {
-height: 100%;
+  height: 100%;
   line-height: 1.5;
   -webkit-font-smoothing: antialiased;
+  font-family: 'Fira Sans', sans-serif;
+  font-family: 'Outfit', sans-serif;
 }
 
 * {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,75 @@
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
-import styled from "styled-components";
+import styled from "styled-components/macro";
 import { login } from "../services/api";
 
-const Wrapper = styled.div``;
-// w-full max-w-screen-md mx-auto rounded-sm h-full text-center
+const FormHead = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+  padding-bottom: 1rem;
+`;
+
+const Form = styled.form`
+  /* width: max-content; */
+  margin: auto;
+  border: dotted 1px silver;
+  border-radius: 2px;
+  padding: 24px;
+`;
+
+const Label = styled.label`
+  display: block;
+  margin-bottom: 16px;
+  font-size: 1.2rem;
+`;
+
+const Input = styled.input`
+  display: block;
+  border: 1px solid black;
+  border-bottom-width: 2px;
+  padding: 6px 8px;
+  margin: 4px 0;
+
+  &::placeholder {
+    color: #a09e9e;
+  }
+
+  &:focus {
+    outline: 2px solid blue;
+    outline-offset: 2px;
+    border-color: transparent;
+  }
+`;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  color: hsl(0, 0%, 100%);
+  padding: 6px 8px;
+  text-transform: uppercase;
+  font-weight: 600;
+  background-color: hsl(0, 100%, 0%);
+  border: none;
+
+  &:focus {
+    outline: 2px solid hsl(130, 50%, 70%);
+    outline-offset: 2px;
+    border: none;
+    background: hsl(
+      129.62264150943398,
+      46.49122807017543%,
+      55.294117647058826%
+    );
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+`;
+
 export const Login = () => {
   const {
     register,
@@ -34,35 +99,37 @@ export const Login = () => {
 
   return (
     <Wrapper>
-      <h1 className="text-lg my-8">Login</h1>
-      <form
-        className="inline-block text-left"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <label className="block">Username</label>
-        <input
-          className="shadow-xl  focus:outline-none focus:ring-2 focus:ring-purple-300"
-          {...register("username", {
-            required: "This field is required!",
-          })}
-        />
-        {errors.username && (
-          <p style={{ color: "red" }}>{errors.username.message}</p>
-        )}
-        <label className="block">Password</label>
-        <input
-          className="shadow-xl  focus:outline-none focus:ring-2 focus:ring-purple-300"
-          type="password"
-          {...register("password", {
-            required: "This field is required!",
-          })}
-        />
-        {errors.password && (
-          <p style={{ color: "red" }}>{errors.password.message}</p>
-        )}
-        <div></div>
-        <input type="submit" value="Login" />
-      </form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormHead>Log In</FormHead>
+        <Label>
+          Username:
+          <Input
+            placeholder="Enter Username"
+            {...register("username", {
+              required: "This field is required!",
+            })}
+          />
+          {errors.username && (
+            <p style={{ color: "red" }}>{errors.username.message}</p>
+          )}
+        </Label>
+
+        <Label>
+          Password:
+          <Input
+            placeholder="********"
+            type="password"
+            {...register("password", {
+              required: "This field is required!",
+            })}
+          />
+          {errors.password && (
+            <p style={{ color: "red" }}>{errors.password.message}</p>
+          )}
+        </Label>
+
+        <SubmitButton type="submit">Log In</SubmitButton>
+      </Form>
     </Wrapper>
   );
 };
